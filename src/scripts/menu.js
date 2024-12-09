3451527f03fe66bcb0b36ec2f393d01be6a91a91
Eli Silk <esilk@me.com>
2024-12-08 21:30:36 -0500

    Built out the homepage and main styles.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,11 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".nav-links").classList.toggle("expanded");
+const navToggle = document.querySelector('[aria-controls="primary-nav"]');
+
+navToggle.addEventListener("click", () => {
+  const navOpened = navToggle.getAttribute("aria-expanded");
+  if (navOpened === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+  console.log("button pressed", navOpened);
 });
